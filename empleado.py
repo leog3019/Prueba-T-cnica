a97efa14526885badb9f17bd9bea119f1ff31180
Leonardo Quishpe <leonardo.quishpe@epn.edu.ec>
2025-11-06 12:12:23 -0500

    Agrega las clases hijas EmpleadoTiempoCompleto y EmpleadoPorHoras

--- empleado.py
+++ empleado.py
@@ -20,3 +20,27 @@ class Empleado:
         print(f"ID: {self._id}")
         print(f"Nombre: {self.get_nombre_completo()}")
         print(f"Salario Base: ${self._salarioBase:,.2f}")
+
+class EmpleadoTiempoCompleto(Empleado):
+
+    def __init__(self, nombre, apellido, id_empleado, salarioBase):
+        super().__init__(nombre, apellido, id_empleado, salarioBase)
+        self._bono = 0.10 # 10%
+
+    def calcularSalario(self):
+        salario_base = super().calcularSalario()
+        salario_total = salario_base + (salario_base * self._bono)
+        return salario_total
+
+    def mostrarInformacion(self):
+        super().mostrarInformacion()
+        print(f"Tipo: Tiempo Completo")
+        print(f"Salario Calculado (con bono): ${self.calcularSalario():,.2f}")
+
+
+class EmpleadoPorHoras(Empleado):
+
+    def __init__(self, nombre, apellido, id_empleado, horasTrabajadas, pagoPorHora):
+        super().__init__(nombre, apellido, id_empleado, 0) 
+        self._horasTrabajadas = horasTrabajadas
+        self._pagoPorHora = pagoPorHora
