118f9c7680c37a8fc7c2a62f24b02f33d038ea1a
Leonardo Quishpe <leonardo.quishpe@epn.edu.ec>
2025-11-06 12:43:22 -0500

    Refactorizacion y documentacion del codigo

--- empleado.py
+++ empleado.py
@@ -1,3 +1,5 @@
+# la estructura basica del programa es crear el empleado basico y de ahi crear las clases hijas que heredan de empleado 
+# cada clase hija tiene su propia forma de calcular el salario y mostrar la informacion
 class Empleado:
 
     def __init__(self, nombre, apellido, id_empleado, salarioBase):
@@ -13,11 +15,11 @@ class Empleado:
     def get_id(self):
         return self._id
 
-    # --- calcular el salario esto se sobreescribe con sus clases hijas
+    # --- calcular el salario esto se sobreescribe con sus clases hijas donde se calcular el salario con bono o por horas
     def calcularSalario(self):
             return self._salarioBase
 
-    def mostrarInformacion(self):
+    def mostrarInformacion(self): # Muestra la información básica del empleado
         print(f"ID: {self._id}")
         print(f"Nombre: {self.get_nombre_completo()}")
         print(f"Salario Base: ${self._salarioBase:,.2f}")
@@ -25,8 +27,8 @@ class Empleado:
 class EmpleadoTiempoCompleto(Empleado):
 
     def __init__(self, nombre, apellido, id_empleado, salarioBase):
-        super().__init__(nombre, apellido, id_empleado, salarioBase)
-        self._bono = 0.10 # 10%
+        super().__init__(nombre, apellido, id_empleado, salarioBase) #llamamos al contructos base de empleado para inicializar los atributos comunes
+        self._bono = 0.10 # 10% bono
 
     def calcularSalario(self):
         salario_base = super().calcularSalario()
@@ -35,7 +37,7 @@ class EmpleadoTiempoCompleto(Empleado):
 
     def mostrarInformacion(self):
 
-        super().mostrarInformacion()
+        super().mostrarInformacion() # usamos super para llamar al metodo de la clase base 
         print(f"Tipo: Tiempo Completo")
         print(f"Salario Calculado (con bono): ${self.calcularSalario():,.2f}")
 
@@ -43,17 +45,18 @@ class EmpleadoTiempoCompleto(Empleado):
 class EmpleadoPorHoras(Empleado):
 
     def __init__(self, nombre, apellido, id_empleado, horasTrabajadas, pagoPorHora):
-        super().__init__(nombre, apellido, id_empleado, 0) 
+        super().__init__(nombre, apellido, id_empleado, 0) # el numero de horas no afecta el salario base por lo que inicializamos con 0
         self._horasTrabajadas = horasTrabajadas
         self._pagoPorHora = pagoPorHora
 
     def calcularSalario(self):
 
-        salario_total = self._horasTrabajadas * self._pagoPorHora
+        salario_total = self._horasTrabajadas * self._pagoPorHora 
         return salario_total
 
     def mostrarInformacion(self):
 
+        #usamos los getters para obtener la informacion del empleado ya que la clase base no tiene salario base por lo que no podemos usar super()
         print(f"ID: {self.get_id()}")
         print(f"Nombre: {self.get_nombre_completo()}")
         print(f"Tipo: Por Horas")
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from empleado import Empleado, EmpleadoTiempoCompleto, EmpleadoPorHoras
+from empleado import Empleado, EmpleadoTiempoCompleto, EmpleadoPorHoras #improtamos las clases desde empleado.py
+
+
+#definimos esta funcion para hacer la prueba de la nomina
 
 def ejecutar_nomina():
     
     emp_completo = EmpleadoTiempoCompleto("Ana", "García", "001", 3000)
     emp_horas = EmpleadoPorHoras("Juan", "Pérez", "002", 40, 15)
-    empleados = [emp_completo, emp_horas]
+    empleados = [emp_completo, emp_horas] # lista de empleados
     
     print("--- INFORME DE NÓMINA DATHA+ ---")
     
+    #recorre toda la lista de empleados para mostrar su informacion y su salario calculado
     for empleado in empleados:
         empleado.mostrarInformacion()
         print("-" * 50)
